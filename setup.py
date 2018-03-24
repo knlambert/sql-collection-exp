99f675ed97d72565b3d339608c753b1b6ee2aa72
Kevin LAMBERT <kevin.lambert@free.fr>
2018-03-24 17:35:52 -0400

    Update Travis CI to push on Pypi.

--- setup.py
+++ setup.py
@@ -6,10 +6,10 @@ from setuptools import setup
 
 setup(
     name=u'sqlcollection',
-    version=u'0.1',
+    version=u'0.1.1',
     packages=[u'sqlcollection', u'sqlcollection.results'],
     install_requires=[u'SQLAlchemy'],
-    download_url = 'https://github.com/knlambert/sqlcollection/archive/0.1.0.tar.gz',
+    download_url = 'https://github.com/knlambert/sqlcollection/archive/0.1.1.tar.gz',
     url=u'https://github.com/knlambert/sql-collection.git',
     keywords=[]
 )
