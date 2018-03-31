945a29e9ba01a5644fe6c48535b47392ff49c21d
Kevin LAMBERT <kevin.lambert@free.fr>
2018-03-31 12:42:14 -0400

    Release 0.1.2.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 
 setup(
     name=u'sqlcollection',
-    version=u'0.1.1',
+    version=u'0.1.2',
     packages=[u'sqlcollection', u'sqlcollection.results'],
     install_requires=[
         u'SQLAlchemy>=1.2,<2'
