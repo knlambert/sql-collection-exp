6023338973735121e5329167ce44cac6ab00403b
Kevin LAMBERT <kevin.lambert@free.fr>
2018-03-31 12:35:48 -0400

    Update setup.py.

--- setup.py
+++ setup.py
@@ -8,8 +8,10 @@ setup(
     name=u'sqlcollection',
     version=u'0.1.1',
     packages=[u'sqlcollection', u'sqlcollection.results'],
-    install_requires=[u'SQLAlchemy'],
-    download_url = 'https://github.com/knlambert/sqlcollection/archive/0.1.1.tar.gz',
+    install_requires=[
+        u'SQLAlchemy>=1.2,<2'
+    ],
+    download_url = u'https://github.com/knlambert/sqlcollection/archive/0.1.1.tar.gz',
     url=u'https://github.com/knlambert/sql-collection.git',
     keywords=[]
 )
