940398cd51682125e47a74f3959961ddc645003a
Kevin LAMBERT <kevin.lambert@free.fr>
2018-09-02 17:37:14 -0400

    Update requirements.

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ Setup file describing how to install.
 from setuptools import setup
 
 setup(
+<<<<<<< HEAD
     name=u'sqlcollection',
     version=u'0.1.4',
     packages=[u'sqlcollection', u'sqlcollection.results'],
@@ -13,5 +14,12 @@ setup(
     ],
     download_url = u'https://github.com/knlambert/sqlcollection/archive/0.1.4.tar.gz',
     url=u'https://github.com/knlambert/sql-collection.git',
+=======
+    name='sqlcollection',
+    version='0.2.0',
+    packages=['sqlcollection', 'sqlcollection.results'],
+    install_requires=["SQLAlchemy>=1.2,<2"],
+    url='https://github.com/knlambert/sql-collection.git',
+>>>>>>> Update requirements.
     keywords=[]
 )
